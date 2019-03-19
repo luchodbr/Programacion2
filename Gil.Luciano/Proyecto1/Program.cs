cbf2247f0f977999e28223c8aa3ddf0ee929b9a9
luchodbr <36521786+luchodbr@users.noreply.github.com>
2019-03-19 12:07:00 -0300

    Add files via upload

---
+++ Gil.Luciano/Proyecto1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // int numero1;
+            //int numero2;
+            // int numero3;
+            Console.WriteLine("Ingrese un nombre");
+            string valor = Console.ReadLine();
+            Console.WriteLine("El nombre es: {0}" ,valor);
+            //int.TryParse(valor, out int valorInt);
+            //Console.WriteLine("{0}", valorInt);
+
+            Console.ReadKey();
+        }
+    }
+}
